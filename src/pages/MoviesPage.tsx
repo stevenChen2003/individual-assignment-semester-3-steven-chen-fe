4a602c7be0fe1d5b5587c5d7411c52108b6d7c69
Steven <i507678>
2023-10-11 02:07:34 +0200

    Made NavBar comp and moviepage for admin

---
+++ src/pages/MoviesPage.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+
+function MoviePage() {
+    /*Work on this tmr
+    const [movieItems, setMovieItems] = useState([]);
+
+    const addItem = (title, description) => {
+        
+    }*/
+
+    return (
+        <div className="container">
+            Welcome to MoviePage
+        </div>
+    )
+}
+
+export default MoviePage;
