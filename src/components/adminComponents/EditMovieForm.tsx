ac3005505fe4ddea7d27d79cb8e06745b5c8122e
Steven <i507678>
2023-11-10 09:22:01 +0100

    change alert and use toast library

--- src/components/adminComponents/EditMovieForm.tsx
+++ src/components/adminComponents/EditMovieForm.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import AdminMovieApi from '../../api/adminApi/AdminMovieApi';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function EditMovieForm(props) {
   const navigate = useNavigate();
@@ -34,89 +36,114 @@ export default function EditMovieForm(props) {
     e.preventDefault();
     AdminMovieApi.updateMovie(movie)
     .then(response => {
-      alert("Update succesfully")
-      navigate('/')
+      showToast("Update succesfully", "success")
+
+      //Delay for 2 sec and then redirect
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
 
     })
     .catch(error => {
-      alert("Update failed")
+      showToast("Update failed", "error")
     })
   }
+
+  const showToast = (message, type) => {
+    toast[type](message, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      style: {
+        background: type === 'success' ? '#4CAF50' : '#FF5733',
+        color: "#ffffff",
+      },
+    });
+  }
+  
 
   useEffect(() => {
     getMovieInformation()
   }, [props.movieId])
   
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Title
-        </label>
-        <input
-          type="text"
-          id="title"
-          className="form-control"
-          value={movie.title}
-          onChange={(e) => setMovie({ ...movie, title: e.target.value })}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          Description
-        </label>
-        <textarea
-          id="description"
-          className="form-control"
-          value={movie.description}
-          onChange={(e) => setMovie({ ...movie, description: e.target.value })}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="genre" className="form-label">
-          Genre
-        </label>
-        <input
-          type="text"
-          id="genre"
-          className="form-control"
-          value={movie.genre}
-          onChange={(e) => setMovie({ ...movie, genre: e.target.value })}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="releaseDate" className="form-label">
-          Release Date
-        </label>
-        <input
-          type="date"
-          id="releaseDate"
-          className="form-control"
-          value={movie.releaseDate}
-          onChange={(e) => setMovie({ ...movie, releaseDate: e.target.value })}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="durationInMin" className="form-label">
-          Duration (in minutes)
-        </label>
-        <input
-          type="number"
-          id="durationInMin"
-          min="0"
-          className="form-control"
-          value={movie.durationInMin}
-          onChange={(e) => setMovie({ ...movie, durationInMin: parseInt(e.target.value) })}
-          required
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Save
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            className="form-control"
+            value={movie.title}
+            onChange={(e) => setMovie({ ...movie, title: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <textarea
+            id="description"
+            className="form-control"
+            value={movie.description}
+            onChange={(e) => setMovie({ ...movie, description: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="genre" className="form-label">
+            Genre
+          </label>
+          <input
+            type="text"
+            id="genre"
+            className="form-control"
+            value={movie.genre}
+            onChange={(e) => setMovie({ ...movie, genre: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="releaseDate" className="form-label">
+            Release Date
+          </label>
+          <input
+            type="date"
+            id="releaseDate"
+            className="form-control"
+            value={movie.releaseDate}
+            onChange={(e) => setMovie({ ...movie, releaseDate: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="durationInMin" className="form-label">
+            Duration (in minutes)
+          </label>
+          <input
+            type="number"
+            id="durationInMin"
+            min="0"
+            className="form-control"
+            value={movie.durationInMin}
+            onChange={(e) => setMovie({ ...movie, durationInMin: parseInt(e.target.value) })}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
+    
   );
 }
