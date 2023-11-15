855abe48530b1ed52529c6d14bd3b2e81dc5c7da
Steven <i507678>
2023-11-15 13:02:46 +0100

    minor fix

--- src/api/AuthApi.tsx
+++ src/api/AuthApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import TokenManager from "./TokenManager";
 
 const AuthAPI = {
-    login: (username, password) => axios.post('http://localhost:8080/tokens', { username, password })
+    login: (email, password) => axios.post('http://localhost:8080/tokens', { email, password })
         .then(response => response.data.accessToken)
         .then(accessToken => TokenManager.setAccessToken(accessToken))
 }
