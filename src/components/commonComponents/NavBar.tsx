d98b064df6051a3877cd724a52e45b6a31d80a36
Steven <i507678>
2023-11-21 14:47:15 +0100

    log out

--- src/components/commonComponents/NavBar.tsx
+++ src/components/commonComponents/NavBar.tsx
@@ -1,29 +1,50 @@
 import React from 'react';
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import TokenManager from '../../api/TokenManager';
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const isLoggedIn = TokenManager.getAccessToken() !== null;
+
+  const handleLogout = () => {
+    TokenManager.clear();
+    navigate('/');
+  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img src={logo} alt="Logo"  width={170} height={55}/>
+          <img src={logo} alt="Logo" width={170} height={55} />
         </Link>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          
+
+        <div className="navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Movie List</Link>
+              <Link className="nav-link active" aria-current="page" to="/">
+                Movie List
+              </Link>
             </li>
+            {isLoggedIn ? (
+              <li className="nav-item">
+                <button className="nav-link btn btn-link" onClick={handleLogout}>
+                  Logout
+                </button>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            )}
           </ul>
-
         </div>
       </div>
     </nav>
