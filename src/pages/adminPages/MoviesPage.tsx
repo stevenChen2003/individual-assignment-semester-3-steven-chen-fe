6209416484a6698580d9ed6e1ad6bc54dbf088b7
Steven <i507678>
2023-11-22 15:27:33 +0100

    combine add to movie table in single page

--- src/pages/adminPages/MoviesPage.tsx
+++ src/pages/adminPages/MoviesPage.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import MovieTable from "../../components/adminComponents/MovieTable";
 import MovieApi from "../../api/adminApi/MovieApi";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import AddMovieForm from "../../components/adminComponents/AddMovieForm";
 
 
 function MoviePage() {
@@ -23,14 +25,41 @@ function MoviePage() {
         .catch(error => console.log(error));
     }
 
+    const handleAddMovie = (newMovie) => {
+    
+        MovieApi.postMovie(newMovie)
+        .then(response => {
+          console.log("Movie added successfully:", response);
+          toast.success('Movie added successfully');
+          refreshMovieList();
+        })
+        .catch(error => {
+          if (error.response.status === 400) {
+            console.log("Movie already exists error:", error.response.data);
+            toast.error("Movie already exists");
+          } else {
+            toast.error("Sorry, something went wrong");
+          }
+        });
+        
+      }
+
 
 
     return (
         <div className="container">
-            <MovieTable movieItems={movieItems} refreshMovieList={refreshMovieList}/>
-            <div className="col-md-6 mt-3">
-                <button className='btn btn-primary'  onClick={navigateToAddMoviePage}>+ Add Movie</button>
-            </div> 
+            <div className="row">
+                <div className="col-9">
+                    <MovieTable movieItems={movieItems} refreshMovieList={refreshMovieList}/>
+                </div>
+                <div className="col-3">
+                    <div className="container">
+                    <h2>Add Movie</h2>
+                    <ToastContainer position="top-center" toastStyle={{ marginTop: '50px' }} />
+                    <AddMovieForm onAddMovie={handleAddMovie}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
