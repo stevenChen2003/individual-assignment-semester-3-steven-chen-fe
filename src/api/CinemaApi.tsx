8d0c195cea6653ea59f8361457b82cbe755112c3
Steven <i507678>
2023-11-30 04:47:10 +0100

    cinema api done need to test it out

---
+++ src/api/CinemaApi.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import TokenManager from "./TokenManager";
+
+axios.defaults.baseURL = "http://localhost:8080";
+
+const CinemaApi = {
+    getCinemas: () => axios.get(`/cinemas`)
+    .then(response => response.data.cinemas),
+
+    getCinema: (cinemaId: any) => axios.get(`/cinemas/${cinemaId}`)
+    .then(response => response.data),
+
+    addCinema: (newCinema: any) => axios.post("/cinemas", newCinema,
+    {
+        headers: { Authorization: `Bearer ${TokenManager.getAccessToken()}` }
+    }),
+
+    updateCinema: (updateCinema: { id: any; }) => axios.put(`/cinemas/${updateCinema.id}`, updateCinema, 
+    {
+        headers: { Authorization: `Bearer ${TokenManager.getAccessToken()}` }
+    }),
+
+    deleteCinema: (cinemaId: any) => axios.delete(`/cinemas/${cinemaId}`,
+    {
+        headers: { Authorization: `Bearer ${TokenManager.getAccessToken()}` }
+    })
+    .then((response) => response.data)
+
+}
+
+export default CinemaApi
