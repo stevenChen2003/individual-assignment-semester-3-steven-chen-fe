9d1288c632f60b2c6036a637f7bd021f8ac0cea1
Steven <i507678>
2023-12-02 05:49:27 +0100

    add works need to combine with edit

--- src/pages/adminPages/CinemasPage.tsx
+++ src/pages/adminPages/CinemasPage.tsx
@@ -2,77 +2,109 @@ import React, { useEffect, useState } from 'react';
 import CinemaSelect from '../../components/adminComponents/CinemaSelect';
 import CinemaApi from '../../api/CinemaApi';
 import CinemaButtons from '../../components/adminComponents/CinemaButtons';
+import { Button } from 'react-bootstrap';
+import CinemaForm from '../../components/adminComponents/CinemaForm';
 
 const CinemaPage = () => {
-  const [cinemas, setCinemas] = useState([]);
-  const [selectedCinema, setSelectedCinema] = useState(null);
-  const [cinema, setCinema] = useState([]);
+ const [cinemas, setCinemas] = useState([]);
+ const [selectedCinema, setSelectedCinema] = useState(null);
+ const [cinema, setCinema] = useState({
+  cinemaId: '',
+  name: '',
+  location: '',
+  numHalls: ''
+ });
+ const [show, setShow] = useState(false);
 
-  const handleSelect = (selectedCinemaId) => {
-    console.log(selectedCinemaId);
-    setSelectedCinema(selectedCinemaId);
+ const handleSelect = (selectedCinemaId) => {
+  console.log(selectedCinemaId);
+  setSelectedCinema(selectedCinemaId);
 
-    if (selectedCinemaId) {
-      // If a cinema is selected, fetch its details
-      CinemaApi.getCinema(selectedCinemaId)
-        .then((response) => {
-          setCinema(response);
-          console.log(response);
-        })
-        .catch((error) => {
-          console.error('Error fetching cinema details:', error);
-        });
-    } else {
-      // If no cinema is selected, clear the cinema details
-      setCinema([]);
-    }
-  };
-
-  const handleEdit = (cinema) => {
-    // Add your edit logic here
-  };
-
-  const handleDelete = () => {
-    console.log(selectedCinema);
-    // Add your delete logic here
-  };
-
-  useEffect(() => {
-    CinemaApi.getCinemas()
+  if (selectedCinemaId) {
+    CinemaApi.getCinema(selectedCinemaId)
       .then((response) => {
-        setCinemas(response);
         console.log(response);
+        setCinema({
+          cinemaId: response.cinemaId,
+          name: response.name,
+          location: response.location,
+          numHalls: response.amountOfHalls
+        });
       })
       .catch((error) => {
-        console.error('Error fetching cinemas:', error);
+        console.error('Error fetching cinema details:', error);
       });
-  }, []);
+  } else {
+    setCinema({
+      cinemaId: '',
+      name: '',
+      location: '',
+      numHalls: ''
+    });
+  }
+ };
 
-  return (
-    <div className='container-fluid m-4'>
-      <div className='row'>
-        <div className='col-md-5 d-flex'>
-          <div className='col-md-9'>
-            <CinemaSelect cinemas={cinemas} onSelect={handleSelect} />
+ const handleGetCinemas = () => {
+  CinemaApi.getCinemas()
+  .then((response) => {
+    setCinemas(response);
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error('Error fetching cinemas:', error);
+  });
+
+ }
+
+ const handleEdit = (cinema) => {
+  // Add your edit logic here
+ };
+
+ const handleDelete = () => {
+  console.log(selectedCinema);
+  CinemaApi.deleteCinema(cinema.cinemaId);
+ };
+
+ const handleClose = () => setShow(false);
+ const handleShow = () => setShow(true);
+
+ 
+
+ useEffect(() => {
+  handleGetCinemas();
+ }, []);
+
+ return (
+  <div className='container-fluid m-4'>
+    <div className='row'>
+      <div className='col-md-5 d-flex'>
+        <div className='col-md-9'>
+          <CinemaSelect cinemas={cinemas} onSelect={handleSelect} />
+          <div className='mt-2'>
+            <Button variant='primary' onClick={handleShow}>
+              Add Cinema
+            </Button>
           </div>
-          {selectedCinema && (
-            <div className='col-md-3'>
-              <CinemaButtons onEdit={handleEdit} onDelete={handleDelete} />
-            </div>
-          )}
         </div>
-        <div className='col-md-7'>
-          {cinema && (
-            <div>
-              <h2>{cinema.name}</h2>
-            </div>
-          )}
+        {selectedCinema && (
+          <div className='col-md-3'>
+            <CinemaButtons onEdit={handleEdit} onDelete={handleDelete} />
+          </div>
+        )}
+        <div>
         </div>
       </div>
+      <div className='col-md-7'>
+        {cinema && (
+          <div>
+            <h2>{cinema.name}</h2>
+          </div>
+        )}
+      </div>
     </div>
-  );
+    <CinemaForm show={show} handleClose={handleClose} onGetCinemas={handleGetCinemas}/>
+  </div>
+ );
 };
 
 export default CinemaPage;
-
-
