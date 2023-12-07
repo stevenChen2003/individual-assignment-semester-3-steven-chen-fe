b16df1ae6c90d3807009d92597a594ab32cacfa8
Steven <i507678>
2023-12-07 04:29:32 +0100

    clean up

--- src/pages/HallPage.tsx
+++ src/pages/HallPage.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import HallLayout from '../components/adminComponents/HallLayout';
-import HallForm from '../components/adminComponents/HallForm';
-import HallApi from '../api/HallApi';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import HallLayout from "../components/adminComponents/HallLayout";
+import HallForm from "../components/adminComponents/HallForm";
+import HallApi from "../api/HallApi";
+import { useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const HallPage = () => {
   const { id } = useParams();
+  const [hallNumber, setHallNumber] = useState(1);
   const [seatPerRow, setSeatPerRow] = useState(5);
   const [numRows, setNumRows] = useState(5);
   const [hallLayout, setHallLayout] = useState([]);
@@ -29,35 +31,40 @@ const HallPage = () => {
 
   const handleAddHall = async () => {
     try {
+      const capacity = seatPerRow * numRows;
+      console.log(capacity)
       const newHall = {
-        cinemaId: id,
-        seatPerRow,
+        hallNumber,
         numRows,
+        seatPerRow,
+        cinemaId: id,
       };
 
       await HallApi.addHall(newHall);
 
-      console.log('Hall added successfully!');
+      console.log("Hall added successfully!");
     } catch (error) {
-      console.error('Error adding hall:', error);
+      console.error("Error adding hall:", error);
     }
   };
 
   return (
-    <div className='container'>
+    <div className="container mt-2">
       <h2>Create Hall</h2>
       <HallForm
+        hallNumber={hallNumber}
+        setHallNumber={setHallNumber} 
         seatPerRow={seatPerRow}
         setSeatPerRow={setSeatPerRow}
         numRows={numRows}
-        setNumRows={setNumRows}
-      />
+        setNumRows={setNumRows}/>
       <HallLayout hallLayout={hallLayout} />
 
-      <button onClick={handleAddHall}>Add Hall</button>
+      <Button variant="primary" onClick={handleAddHall}>
+        Add Hall
+      </Button>
     </div>
   );
 };
 
 export default HallPage;
-
