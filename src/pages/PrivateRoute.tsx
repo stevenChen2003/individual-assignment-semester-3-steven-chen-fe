5cc5ace5ec62f2f64cae2564a3e64404955a0e03
Steven <i507678>
2023-12-08 10:17:58 +0100

    clean up code

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -10,6 +10,9 @@ const PrivateRoute = ({ roles, children }) => {
   // Check if the user has the required roles
   if (claims?.roles?.some(role => roles.includes(role))) {
     return children;
+  } else if (claims != null) {
+    return <Navigate to="/" />;
+
   } else {
     // Redirect to the login page if the user doesn't have the required roles
     return <Navigate to="/login" />;
