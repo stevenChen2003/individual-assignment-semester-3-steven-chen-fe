6f6284d382fd7ee89930123d1aefbbc3023ecaea
Steven <i507678>
2023-12-12 23:58:54 +0100

    made api need to fix it

---
+++ src/api/ShowtimeApi.tsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+import TokenManager from "./TokenManager";
+
+axios.defaults.baseURL = "http://localhost:8080";
+
+const ShowtimeApi = {
+
+}
+
+export default ShowtimeApi;
