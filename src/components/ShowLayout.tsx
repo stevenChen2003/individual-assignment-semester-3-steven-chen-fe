f1fc8e69b3e0701d81456e036c26b3b7941939ab
Steven <i507678>
2024-01-06 20:01:26 +0100

    make a limit for selecting seats

--- src/components/ShowLayout.tsx
+++ src/components/ShowLayout.tsx
@@ -2,53 +2,42 @@ import React, { useEffect, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import "./adminComponents/Show.css";
 
-const ShowLayout = ({ seats }) => {
-  const [selectedSeats, setSelectedSeats] = useState([]);
+const ShowLayout = ({ seats, selectedSeats, handleSeatClick}) => {
+ // Group the seats by row number
+ const groupedSeats = seats.reduce((groups, seat) => {
+  if (!groups[seat.rowNumber]) {
+    groups[seat.rowNumber] = [];
+  }
+  groups[seat.rowNumber].push(seat);
+  return groups;
+ }, {});
 
-  const handleSeatClick = (seatId) => {
-    setSelectedSeats((prevSeats) => {
-      if (prevSeats.includes(seatId)) {
-        return prevSeats.filter((id) => id !== seatId);
-      } else {
-        return [...prevSeats, seatId];
-      }
-    });
-  };
+ useEffect(() => {
+  console.log("Booking", selectedSeats);
+ }, [selectedSeats]);
 
-  // Group the seats by row number
-  const groupedSeats = seats.reduce((groups, seat) => {
-    if (!groups[seat.rowNumber]) {
-      groups[seat.rowNumber] = [];
-    }
-    groups[seat.rowNumber].push(seat);
-    return groups;
-  }, {});
-
-  useEffect(() => {
-    console.log("Booking", selectedSeats);
-  }, [selectedSeats]);
-
-  return (
-    <div className="container">
-      {Object.values(groupedSeats).map((row, rowIndex) => (
-        <Row key={rowIndex}>
-          {row.map((seat) => (
-            <div className="seat" key={seat.seatId}>
-              <Button
-                className={`h-100 ${seat.status == 'UNAVAILABLE' ? 'unavailable-seat' : ''}`}
-                variant={
-                  selectedSeats.includes(seat.seatId) ? "primary" : "secondary"
-                }
-                onClick={() => handleSeatClick(seat.seatId)}
-              >
-                {`${seat.rowNumber}-${seat.seatNumber}`}
-              </Button>
-            </div>
-          ))}
-        </Row>
-      ))}
-    </div>
-  );
+ return (
+  <div className="container">
+    {Object.values(groupedSeats).map((row, rowIndex) => (
+      <Row key={rowIndex}>
+        {row.map((seat) => (
+          <div className="seat" key={seat.seatId}>
+            <Button
+              disabled={seat.status == 'UNAVAILABLE'}
+              className={`h-100 ${seat.status == 'UNAVAILABLE' ? 'unavailable-seat' : ''}`}
+              variant={
+               selectedSeats.includes(seat.seatId) ? "primary" : "secondary"
+              }
+              onClick={() => handleSeatClick(seat.seatId)}
+            >
+              {`${seat.rowNumber}-${seat.seatNumber}`}
+            </Button>
+          </div>
+        ))}
+      </Row>
+    ))}
+  </div>
+ );
 };
 
 export default ShowLayout;
--- src/pages/adminPages/ShowDetailPage.tsx
+++ src/pages/adminPages/ShowDetailPage.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import ShowtimeApi from '../../api/ShowtimeApi';
-import ShowCard from '../../components/adminComponents/ShowCard';
-import ShowLayout from '../../components/ShowLayout';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ShowtimeApi from "../../api/ShowtimeApi";
+import ShowCard from "../../components/adminComponents/ShowCard";
+import ShowLayout from "../../components/ShowLayout";
 
 export default function ShowDetailPage() {
   const { id } = useParams();
   const [showtime, setShowtime] = useState(null);
   const [seats, setSeats] = useState([]);
+  const [selectedSeats, setSelectedSeats] = useState([]);
 
   const getShowInformation = async () => {
     try {
@@ -20,9 +21,9 @@ export default function ShowDetailPage() {
       setSeats(seatsData.seats);
 
       console.log(showtimeData);
-      console.log("Test",seatsData);
+      console.log("Test", seatsData);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
@@ -30,21 +31,37 @@ export default function ShowDetailPage() {
     getShowInformation();
   }, [id]);
 
+  const handleSeatClick = (seatId) => {
+    setSelectedSeats((prevSeats) => {
+      if (prevSeats.includes(seatId)) {
+        return prevSeats.filter((id) => id !== seatId);
+      } else if (prevSeats.length < 5) {
+        return [...prevSeats, seatId];
+      } else {
+        return prevSeats;
+      }
+    });
+  };
+
   return (
-    <div className='container mt-4'>
+    <div className="container mt-4">
       <h2>Show details:</h2>
       <hr></hr>
       {showtime === null ? (
         <p>No information</p>
       ) : (
-        <div className='container'>
-          <div className='row'>
-            <div className='col-5'>
+        <div className="container">
+          <div className="row">
+            <div className="col-5">
               <ShowCard showtime={showtime} />
             </div>
-            <div className='col-7'>
+            <div className="col-7">
               <h3>Bookings:</h3>
-              <ShowLayout seats={seats}/>
+              <ShowLayout
+                seats={seats}
+                selectedSeats={selectedSeats}
+                handleSeatClick={handleSeatClick}
+              />
             </div>
           </div>
         </div>
@@ -52,4 +69,3 @@ export default function ShowDetailPage() {
     </div>
   );
 }
-
