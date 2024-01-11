ce9e1575548328494ce440e4c339d96f6e2e2255
Steven <i507678>
2024-01-11 23:36:32 +0100

    pagination done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Pagination } from 'react-bootstrap';
 import MovieList from '../components/commonComponents/MovieList';
 import FilterBar from '../components/commonComponents/FilterBar';
 import MovieApi from '../api/MovieApi';
+
+const MAX_VISIBLE_PAGES = 5;
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
@@ -9,6 +12,7 @@ const HomePage = () => {
   const [selectedGenre, setSelectedGenre] = useState('');
   const [genres, setGenres] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -29,25 +33,53 @@ const HomePage = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        console.log("Search",searchTerm);
-        console.log("Genre",selectedGenre);
-        const moviesResponse = await MovieApi.searchMovies(searchTerm, selectedGenre, currentPage, 5);
-        console.log("Test",moviesResponse);
+        const moviesResponse = await MovieApi.searchMovies(searchTerm, selectedGenre, currentPage, 2);
         setMovies(moviesResponse.content);
+        setTotalPages(moviesResponse.totalPages);
       } catch (error) {
         console.error('Error fetching movies:', error);
       }
     };
- 
+
     fetchMovies();
   }, [searchTerm, selectedGenre, currentPage]);
 
   const handleSearchChange = (value) => {
     setSearchTerm(value);
+    setCurrentPage(0);
   };
 
   const handleGenreChange = (value) => {
     setSelectedGenre(value);
+    setCurrentPage(0);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page - 1);
+  };
+
+  const renderPaginationItems = () => {
+    const items = [];
+    const startPage = Math.max(1, currentPage - Math.floor(MAX_VISIBLE_PAGES / 2));
+    const endPage = Math.min(startPage + MAX_VISIBLE_PAGES - 1, totalPages);
+
+    if (startPage > 1) {
+      items.push(<Pagination.Prev key="prev" onClick={() => handlePageChange(currentPage)} />);
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      items.push(
+        <Pagination.Item key={i} active={i === currentPage + 1} onClick={() => handlePageChange(i)}>
+          {i}
+        </Pagination.Item>
+      );
+    }
+
+    if (endPage < totalPages) {
+      items.push(<Pagination.Next key="next" onClick={() => handlePageChange(currentPage + 2)} />);
+    }
+
+    return items;
   };
 
   return (
@@ -62,6 +94,7 @@ const HomePage = () => {
         </div>
         <div className="col-md-9">
           <MovieList movies={movies} />
+          <Pagination>{renderPaginationItems()}</Pagination>
         </div>
       </div>
     </div>
@@ -69,6 +102,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
-
-
